eee6821497698bb34313ec4790a4f35fe76c495a
rxdy <rudyalvs@gmail.com>
2026-03-05 10:54:09 +0100

    refacto: dispatching vers des méthodes dédiées par type d'article

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -18,35 +18,43 @@ class GildedRose(object):
         decrement = 2 if "Conjured" in item.name else 1
         item.quality = max(0, item.quality - decrement)
 
+    def _update_sulfuras(self, item):
+        pass
+
+    def _update_aged_brie(self, item):
+        item.quality = min(50, item.quality + 1)
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = min(50, item.quality + 1)
+
+    def _update_backstage_pass(self, item):
+        if item.sell_in <= 0:
+            item.quality = 0
+        else:
+            increment = 1
+            if item.sell_in <= 5:
+                increment = 3
+            elif item.sell_in <= 10:
+                increment = 2
+            item.quality = min(50, item.quality + increment)
+        item.sell_in -= 1
+
+    def _update_normal_item(self, item):
+        self._degrade_normal(item)
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            self._degrade_normal(item)
+
     def update_quality(self):
         for item in self.items:
-            if not self._is_aged_brie(item) and not self._is_backstage_pass(item):
-                if item.quality > 0:
-                    if not self._is_sulfuras(item):
-                        self._degrade_normal(item)
+            if self._is_sulfuras(item):
+                self._update_sulfuras(item)
+            elif self._is_aged_brie(item):
+                self._update_aged_brie(item)
+            elif self._is_backstage_pass(item):
+                self._update_backstage_pass(item)
             else:
-                if item.quality < 50:
-                    item.quality += 1
-                    if self._is_backstage_pass(item):
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality += 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality += 1
-            if not self._is_sulfuras(item):
-                item.sell_in -= 1
-            if item.sell_in < 0:
-                if not self._is_aged_brie(item):
-                    if not self._is_backstage_pass(item):
-                        if item.quality > 0:
-                            if not self._is_sulfuras(item):
-                                self._degrade_normal(item)
-                    else:
-                        item.quality = 0
-                else:
-                    if item.quality < 50:
-                        item.quality += 1
+                self._update_normal_item(item)
 
 
 class Item:
