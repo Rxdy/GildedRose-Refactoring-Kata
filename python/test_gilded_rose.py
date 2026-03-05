a014956982c98354f7ebe1d65e94a6dca5543731
rxdy <rudyalvs@gmail.com>
2026-03-05 09:26:09 +0100

    test: item normal - qualité diminue de 1 avant péremption

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 import unittest
-
 from gilded_rose import Item, GildedRose
 
-
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
 
-        
+    def test_normal_item_quality_decreases_by_1_before_sell_in(self):
+        items = [Item("Normal Item", 10, 20)]
+        GildedRose(items).update_quality()
+        self.assertEqual(19, items[0].quality)
+        self.assertEqual(9, items[0].sell_in)
+
 if __name__ == '__main__':
     unittest.main()
